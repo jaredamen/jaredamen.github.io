ee9e17766003ed70c62d3c6fdb3fa4830bf9ba71
GitHub Action <action@github.com>
2026-04-06 19:52:36 +0000

    Update Basecamp PWA to latest version

--- apps/basecamp/registerSW.js
+++ apps/basecamp/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/apps/basecamp/sw.js', { scope: '/apps/basecamp/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
